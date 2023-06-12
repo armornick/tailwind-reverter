bef8d394dd4fe58b61dd24cfaf497779251c3591
armornick <nick.arnoeyts@mail.com>
2023-06-12 15:12:28 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      className: '',
+      tailwindInput: '',
+      cssOutput: '',
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tailwind Reverter</h1>
+
+    <label for="className">
+      Class Name:
+      <input type="text" name="className">
+    </label>
+    <label for="tailwindInput">
+      Tailwind Input:
+      <input type="text" name="tailwindInput">
+    </label>
+    <button>Convert</button>
+    <hr />
+    
+    <label for="cssOutput">Output:</label>
+    <textarea name="cssOutput" rows="10"></textarea>
+  </div>
+</template>
+
